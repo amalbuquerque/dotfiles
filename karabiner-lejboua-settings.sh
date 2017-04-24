831c20b98b355747b411f9ac2fd954dc9515f8c2
André Albuquerque <andre.albuquerque@onfido.com>
2017-04-24 10:31:46 +0100

    karabiner settings

---
+++ karabiner-lejboua-settings.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set remap.commandTab2expose 1
+/bin/echo -n .
+$cli set remap.copy_paste_winstyle_new_tab 1
+/bin/echo -n .
+$cli set remap.copy_paste_winstyle_reload 1
+/bin/echo -n .
+$cli set remap.find_winstyle_no_term 1
+/bin/echo -n .
+$cli set remap.new_winstyle_no_term 1
+/bin/echo -n .
+$cli set remap.option_tab_to_switch_application 1
+/bin/echo -n .
+$cli set remap.pc_style_copy_paste 1
+/bin/echo -n .
+$cli set remap.pc_style_open 1
+/bin/echo -n .
+$cli set remap.pc_style_prev_next 1
+/bin/echo -n .
+$cli set remap.pclikecontrolarrow_except_vm 1
+/bin/echo -n .
+$cli set remap.pclikehomeend_emacs 1
+/bin/echo -n .
+$cli set remap.pcstyle_close 1
+/bin/echo -n .
+$cli set remap.pointing_controlLeftclick2commandLeftclick 1
+/bin/echo -n .
+$cli set remap.redo_winstyle_no_term 1
+/bin/echo -n .
+$cli set remap.reverse_both_scrolling 1
+/bin/echo -n .
+$cli set remap.select_all_winstyle_no_term 1
+/bin/echo -n .
+$cli set remap.undo_winstyle_no_term 1
+/bin/echo -n .
+$cli set repeat.initial_wait 250
+/bin/echo -n .
+$cli set repeat.wait 16
+/bin/echo -n .
+/bin/echo
