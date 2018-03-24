9cf3234f8c39a42f0fd1e54582e0a45e484e1ae6
André Albuquerque <andre.albuquerque@onfido.com>
2018-03-24 06:50:14 +0000

    Scripts to manage monitors
    Given in i3wm everything is manual, we need this kind of scripts to
    manage monitors.

---
+++ job_dual_monitor_setup_laptop_off.sh
@@ -0,0 +1,2 @@
+xrandr --output DP-1-1 --pos 1080x466
+xrandr --output eDP-1 --pos 3000x666
---
+++ toggle_laptop_monitor.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# turned off if there's no resolution on the screen first line on the xrandr list
+if xrandr | grep -q "eDP-1 connected primary (normal"; then
+    xrandr --output eDP-1 --auto
+else
+    xrandr --output eDP-1 --off
+fi
