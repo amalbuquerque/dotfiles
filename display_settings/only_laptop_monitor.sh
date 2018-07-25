57b7b1d57830e0f5153121c58c0f2dfd068bea7c
amalbuquerque <andre.m.de.albuquerque@gmail.com>
2018-07-25 18:27:32 +0100

    Better multimedia mode, easily change between monitors/laptop

---
+++ display_settings/only_laptop_monitor.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-I-1-1 --off --output DP-1 --off --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off
--- display_settings/toggle_laptop_monitor.sh
+++ display_settings/toggle_laptop_monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # turned off if there's no resolution on the screen first line on the xrandr list
-if xrandr | grep -q "eDP-1 connected primary (normal"; then
+if xrandr | grep -q "eDP-1 connected "; then
     xrandr --output eDP-1 --auto
 else
     xrandr --output eDP-1 --off
