0be6e67b87767cf7860512ab7a2ffdfa614f9185
amalbuquerque <andre.m.de.albuquerque@gmail.com>
2018-08-17 10:04:19 +0100

    Change to default after setting audio mode

--- reactivate_sony_bluetooth_headphones.sh
+++ reactivate_sony_bluetooth_headphones.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pacmd set-card-profile `pacmd list-cards | grep bluez_card -B1 | grep index | awk '{print $2}'` off; sleep 2 ; echo -e "disconnect 50:C9:71:97:A1:86\n quit"|bluetoothctl;sleep 5; echo -e "connect 70:26:05:10:48:B4\n quit"|bluetoothctl; sleep 5; pacmd set-card-profile `pacmd list-cards | grep bluez_card -B1 | grep index | awk '{print $2}'` a2dp_sink
+pacmd set-card-profile `pacmd list-cards | grep bluez_card -B1 | grep index | awk '{print $2}'` off; sleep 2 ; echo -e "disconnect 70:26:05:10:48:B4\n quit"|bluetoothctl;sleep 5; echo -e "connect 70:26:05:10:48:B4\n quit"|bluetoothctl; sleep 5; pacmd set-card-profile `pacmd list-cards | grep bluez_card -B1 | grep index | awk '{print $2}'` a2dp_sink
