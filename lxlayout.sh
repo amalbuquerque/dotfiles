f6d4df9d5c1d9ae7c00c74d94fa89c74cac685e8
amalbuquerque <andre.m.de.albuquerque@gmail.com>
2020-08-31 11:04:11 +0100

    (XKB Bug workaround) Send a fake `Ctrl` after applying the XKB mapping
    Check https://gitlab.freedesktop.org/xorg/app/xkbcomp/-/issues/9 for
    more info.
    
    I was observing this behavior when trying to use the
    `interception_tools` + `space2meta` (adapted to send a RCtrl) only with
    the laptop keyboard.
    
    The behavior was that the custom XKB mapping wasn't being applied to the
    "new" device that was "plugged" (in this case it was the virtual device
    created by the `interception`).

--- lxlayout.sh
+++ lxlayout.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-xkbcomp -I$HOME/projs/personal/xkb_spike -a /home/andre/projs/personal/xkb_spike/neo_map $DISPLAY
+# crazy xkbcomp bug: https://gitlab.freedesktop.org/xorg/app/xkbcomp/-/issues/9
+# the `xdotool key Ctrl` is to force the mapping to be applied, otherwise it only
+# worked with another keyboard plugged in (as in, it didn't work just with the laptop keyboard)
+xkbcomp -w0 -I$HOME/projs/personal/xkb_spike -a /home/andre/projs/personal/xkb_spike/neo_map $DISPLAY && xdotool key Ctrl
 numlockx off
