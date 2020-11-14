7080dbe1398f01982287c719bfa608e35fdfbbd6
André Albuquerque <557562+amalbuquerque@users.noreply.github.com>
2020-11-14 21:20:44 +0000

    Update the lxlayout.sh script with the right `neo_map` path

--- lxlayout.sh
+++ lxlayout.sh
@@ -2,5 +2,5 @@
 # crazy xkbcomp bug: https://gitlab.freedesktop.org/xorg/app/xkbcomp/-/issues/9
 # the `xdotool key Ctrl` is to force the mapping to be applied, otherwise it only
 # worked with another keyboard plugged in (as in, it didn't work just with the laptop keyboard)
-xkbcomp -w0 -I$HOME/projs/personal/xkb_spike -a /home/andre/projs/personal/xkb_spike/neo_map $DISPLAY && xdotool key Ctrl
+xkbcomp -w0 -I$HOME/projs/personal/xkb-custom-layout -a /home/andre/projs/personal/xkb-custom-layout/neo_map $DISPLAY && xdotool key Ctrl
 numlockx off
